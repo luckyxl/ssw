54ef3639d49b726553650b92483300ecaf562fe1
xule <jerry.xu@analyticservice.net>
2018-03-12 11:21:47 +0800

    测试类

--- src/test/java/com/aas/ssw/AtomikosTest.java
+++ src/test/java/com/aas/ssw/AtomikosTest.java
@@ -1,34 +1,40 @@
 package com.aas.ssw;
 
-import com.aas.ssw.business.service.KpiInfoService;
 import com.aas.ssw.business.entity.KpiInfo;
+import com.aas.ssw.business.service.AtomikosService;
+import com.aas.ssw.business.service.KpiInfoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import javax.annotation.Resource;
-
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @SpringBootTest(classes = SswApplication.class,webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class TransactionalTest {
+public class AtomikosTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionalTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AtomikosTest.class);
 
-    @Resource
+    @Autowired(required = false)
     private KpiInfoService kpiInfoService;
+    @Autowired(required = false)
+    private AtomikosService atomikosService;
 
     @Test
-    public void insertKpiInfo() {
+    public void insertKpiInfoTest() {
         KpiInfo kpiInfo = new KpiInfo();
         kpiInfo.setFrequency("0");
         kpiInfo.setLevel("0");
         kpiInfo.setTarget(0.5);
         kpiInfoService.insertKpiInfo(kpiInfo);
         LOGGER.info("插入成功");
+    }
+    @Test
+    public void atomikosTest() {
+        atomikosService.test();
     }
 }
