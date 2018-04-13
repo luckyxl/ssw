3b676688d4ec4f263f1029402896d129adc74f4d
luckyxl <279474386@qq.com>
2018-04-13 22:56:22 +0800

    1、将mysql修改为本地docker 2、升级fastdf依赖版本

--- src/main/java/com/aas/ssw/SswApplication.java
+++ src/main/java/com/aas/ssw/SswApplication.java
@@ -1,10 +1,12 @@
 package com.aas.ssw;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
@@ -13,7 +15,7 @@ import org.springframework.jmx.support.RegistrationPolicy;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)//解决JMX重复注册bean的问题
-//@Import(FdfsClientConfig.class)//获取带有连接池的FastDFS Java客户端，暂时不能用，需要等待jar包升级
+@Import(FdfsClientConfig.class)//获取带有连接池的FastDFS Java客户端，暂时不能用，需要等待jar包升级
 public class SswApplication {
 
     public static void main(String[] args) {
