3e681c663c0554d08390da388ccf96b26cdccc38
luckyxl <github123456>
2018-05-04 14:09:13 +0800

    BaseElasticJob修改为抽象类

--- src/main/java/com/aas/ssw/common/component/BaseElasticJob.java
+++ src/main/java/com/aas/ssw/common/component/BaseElasticJob.java
@@ -1,16 +1,11 @@
 package com.aas.ssw.common.component;
 
-import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import lombok.Data;
 
 @Data
-public class BaseElasticJob implements SimpleJob {
+public abstract class BaseElasticJob implements SimpleJob {
     private String cron;
     private int shardingTotalCount;
     private String shardingItemParameters;
-    @Override
-    public void execute(ShardingContext shardingContext) {
-
-    }
 }
